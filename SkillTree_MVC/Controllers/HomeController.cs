59d97ef51e1eac8eb98dcbb108f307d35a706938
孟芸 蔡 <irene85226@gmail.com>
2019-07-18 21:16:52 +0800

    將記帳本呈現資料的部分改為真 DB

--- SkillTree_MVC/Controllers/HomeController.cs
+++ SkillTree_MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using SkillTree_MVC.Models;
+using SkillTree_MVC.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,25 +10,31 @@ namespace SkillTree_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        
+        AccountService AccSvc = new AccountService();
         public ActionResult Index()
         {
-            List<MoneyModel> MoneyList = new List<MoneyModel>();
-            Random Rnd = new Random();
-            for (int i = 1; i <= 100; i++)
-            {
 
+            var Acc_result = AccSvc.Get();
+            return View(Acc_result);
+            #region HW1 
+            //List<MoneyModel> MoneyList = new List<MoneyModel>();
+            ////Random Rnd = new Random();
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    var result = new MoneyModel()
+            //    {
+            //        SN = i,
+            //        type = Rnd.Next(1, 3).ToString(),
+            //        date = DateTime.Now.AddDays(Rnd.Next(1000)),
+            //        price = Rnd.Next(1, 10000).ToString("#,0"),
+            //    };
+            //    MoneyList.Add(result);
+            //}
 
-                var result = new MoneyModel()
-                {
-                    SN = i,
-                    type = Rnd.Next(1, 3).ToString(),
-                    date = DateTime.Now.AddDays(Rnd.Next(1000)),
-                    price = Rnd.Next(1, 10000).ToString("#,0"),
-                };
-                MoneyList.Add(result);
-            }
+            //return View(MoneyList);
+            #endregion
 
-            return View(MoneyList);
         }
 
         public ActionResult About()
---
+++ SkillTree_MVC/Service/AccountService.cs
@@ -0,0 +1,35 @@
+﻿using SkillTree_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace SkillTree_MVC.Service
+{
+    public class AccountService
+    {
+        public SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
+        public List<MoneyModel>  Get()
+        {
+            var getdata = db.AccountBook.Take(100).ToList();
+            List<MoneyModel> MoneyList = new List<MoneyModel>();
+            var i = 1;
+            foreach (var data in getdata)
+            {
+
+                var result = new MoneyModel();
+                result.SN = i++;
+                result.type = data.Categoryyy == 1 ? "支出" : "收入";
+                result.date = data.Dateee;
+                result.price = data.Amounttt.ToString("#,0");
+                result.others = data.Remarkkk;
+
+                MoneyList.Add(result);
+                
+            }
+            return MoneyList;
+        }
+        
+    }
+}
